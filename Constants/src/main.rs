c4170b0770d2e7a7efb4e832b27893b070809f21
Iteoluwakisi Bello <iteoluwakisibello@Iteoluwakisis-MacBook-Pro.local>
2025-03-01 04:11:34 +0100

    Add Cargo.lock and enhance main.rs with constant examples

--- Constants/src/main.rs
+++ Constants/src/main.rs
@@ -1,3 +1,21 @@
+// Constants are always immutable
+// Constants are declared using the const keyword
+// Constants needs to be annotated with a type
+
 fn main() {
+
+    let  x = 5;
+    const Y: i32 = 10;
+    println!("The value of x is: {}", x);
+    println!("The value of y is: {}", Y);
     println!("Hello, world!");
+    println!("The value of PI is: {}", PI);
+    println!("The value of THREE_HOURS_IN_SECONDS is: {}", THREE_HOURS_IN_SECONDS);
 }
+
+// Constants can be declared in any scope, including the global scope, 
+// which makes them useful for values that many parts of code need to know about.
+
+const PI: f32 = 3.14159;
+const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+
